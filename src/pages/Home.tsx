8a70c1fcb62bc66ecb1264b9b2e42128580e8de0
lars <larsmellick@me.com>
2025-04-14 13:12:14 +1000

    Update logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ function Home() {
   return (
     <div className="home-content">
       <img src={logo} className="home-logo" alt="Band logo of Melbourne tram with SAGA CITY graffiti"/>
+      <button className="sound-button">GIVE ME A SOUND</button>
       <h1>upcoming</h1>
       <ul className="upcoming">
         <li>May 25: Last Chance, Melbourne</li>
