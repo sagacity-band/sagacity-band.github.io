1b90c96090636a94d2bcd5fb66dc825aa643df15
lars <larsmellick@me.com>
2025-04-14 16:21:56 +1000

    Add music section with link

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import '../index.css';
+import { properties } from '../properties';
 
 function Music() {
   return (
     <div className="content">
         <h1>music</h1>
-        <p>Check out some of our original tunes</p>
+        <p>Have a listen to some live recorded demos of our original tunes!</p>
+        <div id="dropboxButton">
+            <a id="dropboxButton"
+              href={properties.dropboxUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="sound-button"
+            >
+                CHECK OUT DEMOS
+            </a>
+        </div>
     </div>
   );
 }
--- src/properties.tsx
+++ src/properties.tsx
@@ -1,5 +1,6 @@
 export const properties = {
     instagramUrl: "https://www.instagram.com/saga.cityband",
     facebookUrl: "https://www.facebook.com/profile.php?id=61566868440580",
-    emailUrl: "mailto:hi@sagacity.au?subject=hi from website"
+    emailUrl: "mailto:hi@sagacity.au?subject=hi from website",
+    dropboxUrl: "https://www.dropbox.com/scl/fo/zyx6yq85fsa3uejrsrjrf/AGvFhjDeK_SqMcvu-OI5LyE?rlkey=rsw9q3617kqshi3leaoqg8h2x&st=llwve8vr&dl=0"
 };
