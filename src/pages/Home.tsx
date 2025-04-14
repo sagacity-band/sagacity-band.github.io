16108fb2093d8ce6438e366e56b3e77e5cde0607
lars <larsmellick@me.com>
2025-04-14 14:17:28 +1000

    Add 'give me sound' button and adjust layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,13 @@ function Home() {
     <div className="home-content">
       <img src={logo} className="home-logo" alt="Band logo of Melbourne tram with SAGA CITY graffiti"/>
       <button className="sound-button">GIVE ME A SOUND</button>
-      <h1>upcoming</h1>
-      <ul className="upcoming">
-        <li>May 25: Last Chance, Melbourne</li>
-      </ul>
+      <hr/>
+      <div className="upcoming">
+        <h1>upcoming</h1>
+        <ul className="gig-list">
+          <li>May 25, 2pm: Last Chance, Melbourne</li>
+        </ul>
+      </div>
     </div>
   );
 }
