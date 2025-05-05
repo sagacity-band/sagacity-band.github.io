2d96a7618de95f8da1126bd1a1b4f0ea0acd87ea
lars <larsmellick@me.com>
2025-05-05 14:22:23 +1000

    Change music page from Dropbox to Soundcloud

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -7,16 +7,13 @@ function Music() {
     <div className="content">
         <h1>music</h1>
         <p>Have a listen to some live recorded demos of our original tunes!</p>
-        <div id="dropboxButton">
-            <a id="dropboxButton"
-              href={properties.dropboxUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="sound-button"
-            >
-                LISTEN TO DEMOS
-            </a>
-        </div>
+        <iframe
+          className="soundcloudFrame"
+          title="Saga City band live demos"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/2015673717&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+          frameBorder="0"
+          scrolling="no">
+        </iframe>
     </div>
   );
 }
